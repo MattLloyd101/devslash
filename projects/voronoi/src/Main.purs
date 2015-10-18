c7addf1aa5c932020a15877441da9895cdc397bc
Matthew Lloyd <Matt.Lloyd101@gmail.com>
2015-10-19 00:27:55 +0100

    Made it flicker dots. Pro skillz.

--- projects/voronoi/src/Main.purs
+++ projects/voronoi/src/Main.purs
@@ -7,6 +7,9 @@ import Data.Int hiding (ceil, floor, round)
 import Data.Maybe
 import Data.Traversable
 
+import DOM.RequestAnimationFrame
+import DOM (DOM())
+
 import Control.Monad.Eff
 import Control.Monad.Eff.Console hiding (error)
 import Control.Monad.Eff.Exception
@@ -17,25 +20,40 @@ import Graphics.Canvas
 import Math hiding (log)
 import Math.Point
 
+
 pointWithin :: forall eff. Rectangle -> Eff (random :: RANDOM | eff) Point
 pointWithin area = do
   x <- randomRange area.x (area.x + area.w)
   y <- randomRange area.y (area.y + area.h)
   return $ { x: x, y: y }
 
-drawCanvas :: forall eff. CanvasElement -> Array Point -> Eff (canvas :: Canvas, console :: CONSOLE | eff) (Array Context2D)
-drawCanvas canvas points = do
-  ctx <- getContext2D canvas
-  let circles = (\p -> { x: p.x, y: p.y, r: 0.5, start: 0.0, end: 2.0 * pi }) <$> points
+drawCircles :: forall eff. Context2D -> Array Arc -> Eff (canvas :: Canvas | eff) (Array Context2D)
+drawCircles ctx circles = do
   setFillStyle "#000000" ctx
   traverse (\a -> fillPath ctx $ arc ctx a) circles
+
+update :: forall eff. Rectangle -> Eff (random :: RANDOM | eff) (Array Arc)
+update { w: width, h: height } = do
+  points <- replicateM 100 $ pointWithin { x: 0.0, y: 0.0, w: width, h: height }
+  return $ (\p -> { x: p.x, y: p.y, r: 0.5, start: 0.0, end: 2.0 * pi }) <$> points
+
+render :: forall eff. CanvasElement -> Rectangle -> Array Arc -> Eff (canvas :: Canvas | eff) (Array Context2D)
+render canvas canvasDims circles = do
+  ctx <- getContext2D canvas
+  clearRect ctx canvasDims
+  drawCircles ctx circles
+
+updateLoop :: forall eff. CanvasElement -> Eff (canvas :: Canvas, random :: RANDOM, dom :: DOM | eff) Unit
+updateLoop canvas = do
+  width <- getCanvasWidth canvas
+  height <- getCanvasHeight canvas
+  let canvasDims = { x: 0.0, y: 0.0, w: width, h: height }
+  circles <- update canvasDims
+  render canvas canvasDims circles
+  requestAnimationFrame $ updateLoop canvas
 
 main = do
   canvas <- getCanvasElementById "canvas"
   case canvas of
     Nothing -> throwException $ error "There must be a canvas element called 'canvas' on the page."
-    Just canvas -> do
-      width <- getCanvasWidth canvas
-      height <- getCanvasHeight canvas
-      points <- replicateM 100 $ pointWithin { x: 0.0, y: 0.0, w: width, h: height }
-      drawCanvas canvas points
+    Just canvas -> requestAnimationFrame $ updateLoop canvas
--- projects/voronoi/src/Math/Parabola.purs
+++ projects/voronoi/src/Math/Parabola.purs
@@ -1,8 +1,22 @@
 module Math.Parabola where
 
+import Prelude
+
+import Math
 import Math.Point
+import Graphics.Canvas
 
-type Focus = Point
-type Directrix = Number
+type Parabola = { focus :: Point, directrix :: Number }
 
--- parabolaToDirectrix focus directrix
+-- | Creates a Bezier Curve from a Focus and a Directrix.
+-- | Directrix here is assumed to be parallel to the Y axis.
+parabolaToBezier :: Parabola -> BezierCurve
+parabolaToBezier { focus: { x: x, y:y }, directrix: d } = do
+  let a = sqrt $ (d * d) - (y * y)
+  let px = x - a
+  let py = 0.0
+  let cp1x = x
+  let cp1y = y + d
+  let cp2x = x
+  let cp2y = 0.0
+  { x: px, y: py, cp1x: cp1x, cp1y: cp1y, cp2x: cp2x, cp2y: cp2y }
